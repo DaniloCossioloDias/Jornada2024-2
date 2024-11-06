2f126cab77b6d8ef5076ea4eb2d46dfc49f9b36b
Matheusopt <114448437+Matheusopt@users.noreply.github.com>
2024-11-06 19:35:47 -0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then(m => m.HomePageModule)
+  }, 
+  {
+    path: 'info',
+    loadChildren: () => import('./tela-final/tela-final.module').then(m => m.TelaFinalPageModule)
   },
 ];
 
