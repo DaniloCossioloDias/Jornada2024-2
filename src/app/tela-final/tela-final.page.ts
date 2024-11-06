6d020895172e445afd5f2e2b6d30106da8147224
Matheusopt <114448437+Matheusopt@users.noreply.github.com>
2024-11-06 19:33:18 -0300

    Add files via upload

---
+++ src/app/tela-final/tela-final.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tela-final',
+  templateUrl: './tela-final.page.html',
+  styleUrls: ['./tela-final.page.scss'],
+})
+export class TelaFinalPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
